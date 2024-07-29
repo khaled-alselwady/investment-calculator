1a56d05faed2bfba82cfbea8ad38ecb53b4e4b32
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-30 01:00:06 +0300

    feat(user-input): handle form submission

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
+  imports: [FormsModule],
 })
-export class UserInputComponent {}
+export class UserInputComponent {
+  onSubmit() {
+    console.log('Submitted!');
+  }
+}
