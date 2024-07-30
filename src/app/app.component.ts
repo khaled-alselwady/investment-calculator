9ad94212c4c051c3cfa76c024bfe2304c0237f8a
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-30 22:07:13 +0300

    feat(app): use signal in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import type { InvestmentInput } from './investment-input.model';
@@ -12,7 +12,7 @@ import { AnnualData } from './annual-data.model';
   imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
-  resultsData?: AnnualData[];
+  resultsData = signal<AnnualData[] | undefined>(undefined);
   onCalculateInvestmentResults(date: InvestmentInput) {
     const { initialInvestment, expectedReturn, annualInvestment, duration } =
       date;
@@ -35,6 +35,6 @@ export class AppComponent {
       });
     }
 
-    this.resultsData = annualData;
+    this.resultsData.set(annualData);
   }
 }
